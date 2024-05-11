68fcdd750dd17fb4fd76aa1defc2c42b30b8353b
fahlanlogic <109933376+fahlanlogic@users.noreply.github.com>
2024-05-11 12:39:36 +0700

    Fahdi/category (#17)
    * complete api categories
    
    * resolve conflict yarn.lock
    
    * edit readme.md
    
    * Revert "resolve conflict yarn.lock"
    
    This reverts commit 513542e3c28cf8ae66c843c9636e9b0ed388c800.

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,12 +1,60 @@
 const prisma = require('../libs/prisma')
 
 class CategoryService {
-  static async getAll (req, res, next) {
+  static getAll = async () => {
     try {
       const category = await prisma.category.findMany()
-      return { message: 'get all category', category }
+      return category
     } catch (error) {
       console.log(error)
+      throw error
+    }
+  }
+
+  static create = async (params) => {
+    try {
+      await prisma.category.create({ data: { name: params } })
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static update = async (id, name) => {
+    try {
+      await prisma.category.update({ where: { id }, data: { name } })
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static findName = async (params) => {
+    try {
+      const findName = await prisma.category.findFirst({ where: { name: params } })
+      return findName
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static findUnique = async (id) => {
+    try {
+      const findId = await prisma.category.findUnique({ where: { id: id } })
+      return findId
+    } catch (error) {
+      console.log('Error: ', error)
+      throw error
+    }
+  }
+
+  static delete = async (id) => {
+    try {
+      const deleteById = await prisma.category.delete({ where: { id: id } })
+      return deleteById
+    } catch (error) {
+      console.log('Error: ', error)
       throw error
     }
   }
