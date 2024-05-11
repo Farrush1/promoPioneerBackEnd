6b89d79bc95c18dd13bebef54f365fdb652ee588
Muhammad Syaifullah Al Arief <68526285+arief2020@users.noreply.github.com>
2024-05-11 16:58:29 +0700

    Weli/users (#18)
    * test
    
    * user/weli
    
    * resolve conflict
    
    ---------
    
    Co-authored-by: DESKTOP-IB5SN3J\Weli Erlina <welierlina08@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+//const cookieParser = require('cookie-parser');
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const dotenv = require('dotenv')
