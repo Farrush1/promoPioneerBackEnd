dcd7242770b7f3a5bcdf0d8455a860ae96d7184a
Muhammad Syaifullah Al Arief <68526285+arief2020@users.noreply.github.com>
2024-05-15 09:26:04 +0700

    Luthfi/product (#21)
    * test product
    
    * fitur product
    
    ---------
    
    Co-authored-by: Kungpao-pixel <azharretro46@gmail.com>

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,42 +1,107 @@
+const prisma = require('../libs/prisma')
+
 const ProductService = require('../services/productService')
 const cloudinaryUpload = require('../libs/cloudinary')
 const prisma = require('../libs/prisma')
 class ProductController {
-  static async getAll (req, res, next) {
+  static async getAllProducts(req, res, next) {
     try {
-      const product = await ProductService.getAll()
-      return res.status(200).json(product)
+      const page = parseInt(req.query.page) || 1
+      const limit = parseInt(req.query.limit) || 10
+
+      if (page < 1 || limit < 1) {
+        return res.status(400).json({ message: "Page and limit must bigger than 0." });
+      }
+
+      const startIndex = (page - 1) * limit
+
+      const products = await prisma.product.findMany({
+        skip: startIndex,
+        take: limit,
+      })
+
+      const totalProducts = await prisma.product.count()
+
+      const totalPages = Math.ceil(totalProducts / limit)
+
+      return res.status(200).json({
+        currentPage: page,
+        totalPages: totalPages,
+        totalProducts: totalProducts,
+        products: products,
+      })
     } catch (error) {
+      console.error("Error get all products:", error)
       next(error)
     }
   }
 
-  static async store (req, res, next) {
+  static async getProductById(req, res, next) {
+    try {
+      const productId = req.params.id
+      const product = await ProductService.getProductById(productId)
+      if (!product) {
+        return res.status(404).json({ message: 'Product not found' })
+      }
+      res.status(200).json(product)
+    } catch (error) {
+      next(error)
+      console.error("Error get product by id:", error)
+      res.status(500).json({ message: error.message })
+    }
+  }
+
+  static async updateProduct(req, res, next) {
+    try {
+      const productId = req.params.id
+      const product = await ProductService.updateProduct(productId, req.body)
+      res.status(200).json(product)
+    } catch (error) {
+      next(error)
+      res.status(500).json({ message: error.message })
+    }
+  }
+
+  static async deleteProduct(req, res, next) {
+    try {
+      const productId = req.params.id
+      const product = await ProductService.deleteProduct(productId)
+      res.status(200).json(product)
+    } catch (error) {
+      next(error)
+      res.status(500).json({ message: error.message })
+    }
+  }
+
+  static async store(req, res, next) {
     try {
       const params = {
         file: req.file,
-        body: req.body
+        body: req.body,
       }
       const product = await ProductService.store(params)
       return res.status(200).json(product)
     } catch (error) {
+      console.log(error)
       next(error)
     }
   }
 
-  static async upload (req, res) {
+  static async upload(req, res) {
     try {
       const result = await cloudinaryUpload(req.file.path)
       res.status(200).json({
         success: true,
         message: 'Uploaded!',
-        url: result
+
+        url: result.url,
+
       })
     } catch (error) {
       console.error(error)
       res.status(500).json({
         success: false,
-        message: 'Error'
+        message: 'Error',
       })
     }
   }
--- router/product/index.js
+++ router/product/index.js
@@ -1,9 +1,15 @@
+
 const express = require('express')
 const ProductController = require('../../controllers/productController')
 const upload = require('../../middleware/multer')
 const router = express.Router()
-router.get('/', ProductController.getAll)
-router.delete('/', ProductController.delete)
+
+
+router.get('/', ProductController.getAllProducts);
+router.get('/:id', ProductController.getProductById)
+router.put('/:id', ProductController.updateProduct)
+router.delete('/:id', ProductController.deleteProduct)
+
 router.post('/', upload.single('product_image'), ProductController.store)
 // ignore
 router.post('/upload', upload.single('image'), ProductController.upload)
