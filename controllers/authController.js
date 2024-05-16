13281a3196bd060e808edf214d7e46ac0fea27f9
arief2020 <msyaifullahalarief@gmail.com>
2024-05-16 21:08:30 +0700

    forgot password and reset password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const AuthService = require('../services/authService')
 
 class AuthController {
-  static async register (req, res, next) {
+  static async register(req, res, next) {
     try {
       const user = await AuthService.register(req.body)
       res.status(200).json(user)
@@ -10,7 +10,7 @@ class AuthController {
     }
   }
 
-  static async login (req, res, next) {
+  static async login(req, res, next) {
     try {
       const user = await AuthService.login(req.body, res)
       res.status(200).json(user)
@@ -19,7 +19,25 @@ class AuthController {
     }
   }
 
-  static async logout (req, res, next) {
+  static async forgotPassword(req, res, next) {
+    try {
+      const user = await AuthService.forgotPassword(req.body)
+      res.status(200).json(user)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async resetPassword(req, res, next) {
+    try {
+      const user = await AuthService.resetPassword(req.body)
+      res.status(200).json(user)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async logout(req, res, next) {
     try {
       const user = await AuthService.logout(req, res)
       res.status(200).json(user)
--- router/auth/index.js
+++ router/auth/index.js
@@ -7,6 +7,8 @@ const router = express.Router()
 router.post('/register', AuthController.register)
 
 router.post('/login', AuthController.login)
+router.post('/forgot-password', AuthController.forgotPassword)
+router.post('/reset-password', AuthController.resetPassword)
 
 router.post('/logout', AuthController.logout)
 
