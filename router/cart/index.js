ff9998bc4089df02713efbdc080a29f1e768cb57
Muhammad Syaifullah Al Arief <68526285+arief2020@users.noreply.github.com>
2024-05-16 21:11:45 +0700

    Arief/reset password (#27)
    * checkout api
    
    * bug fixing and seeding
    
    * bug fixing and seeding
    
    * kakak
    
    * add cors
    
    * fix seeding
    
    * lint
    
    * forgot password and reset password

--- router/cart/index.js
+++ router/cart/index.js
@@ -5,7 +5,5 @@ const router = express.Router()
 router.get('/', CartController.getAll)
 router.post('/cart-items', CartController.store)
 router.delete('/cart-items/:id', CartController.destroy)
-// router.post('/', CategoryController.create)
-// router.put('/:id', CategoryController.update)
 
 module.exports = router
