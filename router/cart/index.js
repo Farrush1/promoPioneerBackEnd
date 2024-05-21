7bc57518dd06e16e1e590b745bfc396225d842ae
fahlanlogic <alanfahdi@gmail.com>
2024-05-21 22:50:30 +0700

    api : add method put for cart-items

--- router/cart/index.js
+++ router/cart/index.js
@@ -6,6 +6,7 @@ const router = express.Router()
 router.use(AuthMiddleware.authenticate)
 router.get('/', CartController.getAll)
 router.post('/cart-items', CartController.store)
+router.put('/cart-items/:id', CartController.update)
 router.delete('/cart-items/:id', CartController.destroy)
 
 module.exports = router
